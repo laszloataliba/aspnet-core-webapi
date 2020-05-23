4f49e03e3a3ca22b1912a193e1ea655c28365bdf
Laszlo Ataliba <laszloataliba@gmail.com>
2020-05-23 17:15:20 -0300

    Commit inicial

---
+++ APIs/Controllers/PalavrasController.cs
@@ -0,0 +1,63 @@
+﻿using APIs.DataBase;
+using APIs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIs.Controllers
+{
+    [Route("api/Palavras")]
+    public class PalavrasController : ControllerBase
+    {
+        private readonly APIContext _banco;
+
+        public PalavrasController(APIContext banco)
+        {
+            _banco = banco;
+        }
+
+        [Route(""),
+         HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_banco.Palavras);
+        }
+
+        [Route("{id}"),
+         HttpGet]
+        public IActionResult Get(int id)
+        {
+            return Ok(_banco.Palavras.Find(id));
+        }
+
+        [Route(""),
+         HttpPost]
+        public IActionResult Post(Palavra palavra)
+        {
+            _banco.Palavras.Add(palavra);
+
+            return Ok();
+        }
+
+        [Route("{id}"),
+         HttpPut]
+        public IActionResult Put(int id, Palavra palavra)
+        {
+            palavra.Id = id;
+            _banco.Palavras.Update(palavra);
+
+            return Ok();
+        }
+
+        [Route("{id}"), 
+         HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            _banco.Palavras.Remove(_banco.Palavras.Find(id));
+
+            return Ok();
+        }
+    }
+}
---
+++ APIs/DataBase/APIContext.cs
@@ -0,0 +1,20 @@
+﻿using APIs.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIs.DataBase
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options) : 
+            base(options)
+        {
+
+        }
+
+        public DbSet<Palavra> Palavras { get; set; }
+    }
+}
